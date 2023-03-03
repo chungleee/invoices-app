a8c9b702851abadb548e0daefd7d23b60775dde5
Leon Chung <leon.karho.chung@gmail.com>
2023-03-03 15:39:39 -0500

    turn logo into router link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import { Link } from "wouter";
 import MoonIcon from "./MoonIcon";
 import PieIcon from "./PieIcon";
 
 const Navbar = () => {
 	return (
 		<nav className='h-20 bg-gray-800 flex items-center justify-between'>
-			<div className='rounded-br-lg rounded-tr-lg border-solid bg-purple-500 w-20 h-full relative'>
-				<div className='h-2/4'></div>
-				<div className='h-2/4 rounded-br-lg rounded-tl-lg bg-purple-300'></div>
-				<span className='inline-block absolute top-[35%] left-[30%]'>
-					<PieIcon />
-				</span>
-			</div>
+			<Link href='/'>
+				<a className='rounded-br-lg rounded-tr-lg border-solid bg-purple-500 w-20 h-full relative'>
+					<div className='h-2/4'></div>
+					<div className='h-2/4 rounded-br-lg rounded-tl-lg bg-purple-300'></div>
+					<span className='inline-block absolute top-[35%] left-[30%]'>
+						<PieIcon />
+					</span>
+				</a>
+			</Link>
 
 			<span className='ml-auto mr-8'>
 				<MoonIcon />

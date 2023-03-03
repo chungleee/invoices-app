9636c72e8bb80b2f156a64f3f6573e4dfc629ee8
Leon Chung <leon.karho.chung@gmail.com>
2023-03-03 15:40:05 -0500

    home page setup - display text and create invoice router link btn

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,22 @@
+import { Link } from "wouter";
+import { AiFillPlusCircle } from "react-icons/ai";
+
 const Home = () => {
 	return (
-		<div>this is the home page where you'll see a list of all invoices</div>
+		<main className='px-6 py-8'>
+			<div className='flex justify-between items-center'>
+				<div>
+					<h1 className='font-bold text-xl'>Invoices</h1>
+					<h2 className='font-thin text-sm text-[#888EB0]'>No invoices</h2>
+				</div>
+
+				<Link href='/create-invoice'>
+					<a className='flex justify-evenly capitalize text-sm font-semibold items-center rounded-full w-[90px] h-[42px] bg-[#7C5DFA] text-white'>
+						<AiFillPlusCircle /> new
+					</a>
+				</Link>
+			</div>
+		</main>
 	);
 };
 

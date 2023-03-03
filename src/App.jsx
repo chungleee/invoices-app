6ed85e5111a6d9ca06656d8af84f9ff0ac9381a8
Leon Chung <leon.karho.chung@gmail.com>
2023-03-03 15:20:50 -0500

    configure wouter routes with dummy page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Navbar from "./components/Navbar";
+import { Route } from "wouter";
+import Home from "./pages/Home";
+import CreateInvoice from "./pages/CreateInvoice";
+
 function App() {
-	return <div>hello world</div>;
+	return (
+		<div>
+			<Navbar />
+
+			<Route path='/' component={Home} />
+			<Route path='/create-invoice' component={CreateInvoice} />
+		</div>
+	);
 }
 
 export default App;

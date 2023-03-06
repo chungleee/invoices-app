5f80053c25e7d8a975cd88a3f355aaa89f4b9ac5
Leon Chung <leon.karho.chung@gmail.com>
2023-03-06 15:08:51 -0500

    mobile version of home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,13 @@ const Home = () => {
 	const [invoices, setInvoices] = useState([]);
 
 	return (
-		<main className='px-6 py-8'>
+		<main className='px-6 py-8 h-[calc(100vh-80px)] flex flex-col'>
 			<div className='flex justify-between items-center'>
 				<div>
 					<h1 className='font-bold text-xl'>Invoices</h1>
-					<h2 className='font-thin text-sm text-[#888EB0]'>No invoices</h2>
+					<h2 className='font-thin text-sm text-[#888EB0]'>
+						{!invoices.length ? "No invoices" : `${invoices.length} invoices`}
+					</h2>
 				</div>
 
 				<Link href='/create-invoice'>
@@ -20,9 +22,20 @@ const Home = () => {
 				</Link>
 			</div>
 
-			<section>
+			<section className='w-2/3 grow flex items-center mx-auto'>
 				{!invoices.length ? (
-					<h1>There is nothing here</h1>
+					<div className='text-center'>
+						<img
+							src='../../public/assets/invoice.svg'
+							alt='No invoices image'
+						/>
+						<h2 className='font-bold text-xl my-4'>There is nothing here</h2>
+						<h3 className='font-thin text-sm'>
+							Create an invoice by clicking the{" "}
+							<span className='font-bold text-gray-400'>New</span> button and
+							get started
+						</h3>
+					</div>
 				) : (
 					<h1>here are your invoices</h1>
 				)}

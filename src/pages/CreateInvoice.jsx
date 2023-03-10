3c4518fd5f2b3b06860d4c2e1355be81c74c9d11
Leon Chung <leon.karho.chung@gmail.com>
2023-03-10 15:10:27 -0500

    refactor to use form context - styling for bill to section

--- src/pages/CreateInvoice.jsx
+++ src/pages/CreateInvoice.jsx
@@ -1,16 +1,27 @@
 import { Link } from "wouter";
-import { useForm } from "react-hook-form";
+import { useForm, FormProvider } from "react-hook-form";
 import LeftArrowIcon from "../components/LeftArrowIcon";
 import InputField from "../components/InputField";
 
 const CreateInvoice = () => {
-	const { register, handleSubmit, control } = useForm();
-	const onSubmit = (data) => {
-		console.log("form data: ", data);
+	const methods = useForm();
+
+	const onSubmit = ({
+		streetAddress,
+		city,
+		postalCode,
+		country,
+		...billTo
+	}) => {
+		const from = { streetAddress, city, postalCode, country };
+		const to = { ...billTo };
+
+		console.log("from obj: ", from);
+		console.log("to obj: ", to);
 	};
 
 	return (
-		<main className='py-6	px-4'>
+		<main className='pt-6	px-4'>
 			<Link href='/'>
 				<a href='/' className='flex items-center w-fit'>
 					<span>
@@ -22,31 +33,77 @@ const CreateInvoice = () => {
 			<h1 className='font-bold text-2xl my-4'>New Invoice</h1>
 
 			<section>
-				<h2 className='font-bold text-sm text-purple-500 mb-6'>Bill From</h2>
-				<form
-					onSubmit={handleSubmit(onSubmit)}
-					className='grid grid-cols-2 gap-3 gap-x-4'
-				>
-					<InputField
-						className='col-span-2'
-						register={register}
-						label='Street Address'
-						name='streetAddress'
-					/>
-					<InputField register={register} label='City' name='city' />
-					<InputField
-						register={register}
-						label='Postal Code'
-						name='postalCode'
-					/>
-					<InputField
-						className='col-span-2'
-						register={register}
-						label='Country'
-						name='country'
-					/>
-					<button type='submit'>Submit</button>
-				</form>
+				<FormProvider {...methods}>
+					<form
+						onSubmit={methods.handleSubmit(onSubmit)}
+						className='grid grid-cols-2 gap-4'
+					>
+						<h2 className='font-bold text-sm text-purple-500'>Bill From</h2>
+						<InputField
+							className='col-span-2'
+							label='Street Address'
+							name='streetAddress'
+						/>
+						<InputField label='City' name='city' />
+						<InputField label='Postal Code' name='postalCode' />
+						<InputField className='col-span-2' label='Country' name='country' />
+
+						<h2 className='font-bold text-sm text-purple-500'>Bill To</h2>
+						<InputField
+							className='col-span-2'
+							label="Client's Name"
+							name='clientName'
+						/>
+						<InputField
+							className='col-span-2'
+							label="Client's Email"
+							name='clientEmail'
+						/>
+						<InputField
+							className='col-span-2'
+							label='Street Address'
+							name='clientStreetAddress'
+						/>
+						<InputField label='City' name='clientCity' />
+						<InputField label='Postal Code' name='clientPostalCode' />
+						<InputField
+							className='col-span-2'
+							label='Country'
+							name='clientCountry'
+						/>
+						<InputField
+							className='col-span-2'
+							label='Invoice day'
+							name='invoiceDay'
+						/>
+						<InputField
+							className='col-span-2'
+							label='Project Description'
+							name='projectDescription'
+						/>
+						<div className='col-span-2 flex justify-between py-4 -mx-4 px-4'>
+							<button
+								className='rounded-full px-4 py-4 bg-[#F9FAFE] text-[#7E88C3] text-sm font-semibold'
+								type='button'
+							>
+								Discard
+							</button>
+							<button
+								className='rounded-full px-4 py-4 bg-[#373B53] text-[#888EB0] text-sm font-semibold'
+								type='button'
+							>
+								Save as draft
+							</button>
+							<button
+								className='rounded-full px-4 py-4 bg-[#7C5DFA] text-sm font-semibold text-white'
+								type='button'
+								onClick={methods.handleSubmit(onSubmit)}
+							>
+								Save & Send
+							</button>
+						</div>
+					</form>
+				</FormProvider>
 			</section>
 		</main>
 	);
